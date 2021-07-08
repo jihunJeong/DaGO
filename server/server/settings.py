360741283ce03799a4d0e454e057751c621bc0d4
jihunJeong <jihun.jeong219@gmail.com>
2021-07-08 22:13:15 +0900

    Add DB info

--- server/server/settings.py
+++ server/server/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'graduatedb',
+        'USER': 'admin',
+        'PASSWORD': 'hyrecproject16',
+        'HOST': 'graduate-project.c9rpxmt946al.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
     }
 }
 
