4ca65b9dce9752747174a57303718d2b14a2a72b
jihun Jeong <jihun.jeong219@gmail.com>
2021-07-14 18:09:14 +0900

    Update DB setting and Add new Html

--- E-shop/shopping_mall/shopping_mall/settings.py
+++ E-shop/shopping_mall/shopping_mall/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'uzpf3=&z3(bgm9l=s3f&0avx=!-ytgfcg4j%8ku9+_d)aze0#_'
 DEBUG = True
 
 #ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['localhost','127.0.0.1','54.180.154.189']
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 # Application definition
 
@@ -78,27 +78,20 @@ WSGI_APPLICATION = 'shopping_mall.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'shop',
-        'USER': 'root',
-        'PASSWORD': '0000',
-        'HOST': 'localhost',
+        'NAME': 'graduatedb',
+        'USER': 'admin',
+        'PASSWORD': 'hyrecproject16',
+        'HOST': 'graduate-project.c9rpxmt946al.ap-northeast-2.rds.amazonaws.com',
         'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
     }
 }
-'''
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
