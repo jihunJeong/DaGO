4e07a8f47680cfdbeb264afe2124568ac5ae06fa
HyunJunLee-Hi <nejukja@gmail.com>
2021-08-19 15:21:12 +0900

    order 일부 수정

--- E-shop/shopping_mall/order/forms.py
+++ E-shop/shopping_mall/order/forms.py
@@ -1,15 +1,14 @@
 from django import forms
 from user.models import User
-from product.models import Product
+from product.models import Item
 from .models import Order
-
 
 class OrderForm(forms.Form):
     quantity = forms.IntegerField(
         error_messages={
             'required':'수량을 입력해주세요.'
         }, label="수량")
-    product = forms.IntegerField(
+    product = forms.CharField(
         error_messages={
             'required':'상품을 선택해주세요.'
         },
@@ -17,16 +16,13 @@ class OrderForm(forms.Form):
     )
 
     def clean(self):
-
         cleaned_data = super().clean()
         quantity = cleaned_data.get('quantity')
         product = cleaned_data.get('product')
 
         if quantity and product:
-            product = Product.objects.get(pk=product)
-            if product.stock < quantity:
-                self.add_error('quantity', '재고보다 많은 주문은 할 수 없습니다.')
-                return
+            #product = Item.objects.get(pk=product)
+            product = Item.objects.all()
             if quantity < 0:
                 self.add_error('quantity', '0개 이하는 주문할 수 없습니다.')
         else:
