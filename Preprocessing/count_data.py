c9f5d0bc7b90759c4bdb99998b42fb174c9778c4
jihunJeong <jihun.jeong219@gmail.com>
2021-08-22 21:55:50 +0900

    개인화  Content 시작

---
+++ Preprocessing/count_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+if __name__ == "__main__":
+    data_path = "../data/"
+
+    count = 0    
+    review_reader = pd.read_csv(data_path+'item_2018.csv')
+    print(len(review_reader))
+   
+    
+    
+    
+
+    
